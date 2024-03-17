eed43a97c0eae3d18598a478b12efc1ea62e601a
Bill Van Leeuwen <billvanleeuwen424@gmail.com>
2024-03-17 18:59:01 -0400

    added rand nums and guessing

--- ch2/src/main.rs
+++ ch2/src/main.rs
@@ -1,15 +1,31 @@
-use std::io;
+use rand::Rng;
+use std::{cmp::Ordering, io};
 
 fn main() {
+    let rand_number = rand::thread_rng().gen_range(1..=100);
+
     println!("Guess the number!");
 
-    println!("Please input your gues:");
+    loop {
+        println!("Please input your gues:");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read line"); // crash if error
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line"); // crash if error
 
-    println!("You guessed: {guess}");
+        let guess: u32 = guess.trim().parse().expect("Please enter a number.");
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&rand_number) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too Large"),
+            Ordering::Equal => {
+                println!("You got it!");
+                break;
+            }
+        }
+    }
 }

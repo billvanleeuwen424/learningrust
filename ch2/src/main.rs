18e456d957edc2fc71bbffee6bf39e56e297a549
Bill Van Leeuwen <billvanleeuwen424@gmail.com>
2024-03-17 19:02:19 -0400

    added input handling

--- ch2/src/main.rs
+++ ch2/src/main.rs
@@ -1,5 +1,6 @@
 use rand::Rng;
-use std::{cmp::Ordering, io};
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     let rand_number = rand::thread_rng().gen_range(1..=100);
@@ -15,7 +16,10 @@ fn main() {
             .read_line(&mut guess)
             .expect("failed to read line"); // crash if error
 
-        let guess: u32 = guess.trim().parse().expect("Please enter a number.");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {guess}");
 

d47b1c4d2347aca201d55da202f7f47b9fb6f99a
Bill Van Leeuwen <billvanleeuwen424@gmail.com>
2024-03-25 20:49:19 -0400

    got the first one done

--- ch4/src/main.rs
+++ ch4/src/main.rs
@@ -1,11 +1,27 @@
 /*
-Here’s a small programming problem: write a function that takes a 
-    string of words separated by spaces and returns the first 
-    word it finds in that string. If the function doesn’t find 
-    a space in the string, the whole string must be one word, 
+Here’s a small programming problem: write a function that takes a
+    string of words separated by spaces and returns the first
+    word it finds in that string. If the function doesn’t find
+    a space in the string, the whole string must be one word,
     so the entire string should be returned.
 */
 
 fn main() {
-    println!("Hello, world!");
+
+    let s = String::from("Hello there!");
+    println!("{}", first_word(&s));
+}
+
+/// returns the index of the end of the first word
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    // loop over bytes to find a space
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
 }

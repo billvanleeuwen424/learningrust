234e784cfb19b19176c7d52925d8fd6ce9eaa890
Bill Van Leeuwen <billvanleeuwen424@gmail.com>
2024-03-25 20:32:09 -0400

    starting ch4 programming problem

---
+++ ch4/src/main.rs
@@ -0,0 +1,11 @@
+/*
+Here’s a small programming problem: write a function that takes a 
+    string of words separated by spaces and returns the first 
+    word it finds in that string. If the function doesn’t find 
+    a space in the string, the whole string must be one word, 
+    so the entire string should be returned.
+*/
+
+fn main() {
+    println!("Hello, world!");
+}
